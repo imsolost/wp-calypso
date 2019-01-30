9101c3f1fe1a190a93f956e0690ded25be86dba4
Elio Rivero <eliorivero@gmail.com>
2019-01-30 16:44:00 -0300

    Publicize: add a notice in the panel displayed in Gutenberg informing users that they have to reauthenticate with LinkedIn along with a link so users can go to the Sharing page to reauthenticate

--- client/gutenberg/extensions/publicize/store/selectors.js
+++ client/gutenberg/extensions/publicize/store/selectors.js
@@ -8,3 +8,16 @@
 export function getFailedConnections( state ) {
 	return state.filter( connection => ! connection.test_success );
 }
+
+/**
+ * Returns Publicize connections that require reauthentication from users. For example, when LinkedIn switched its API from v1 to v2.
+ *
+ * @param {Object} state State object.
+ *
+ * @return {Array} List of connections.
+ */
+export function getMustReauthConnections( state ) {
+	return state
+		.filter( connection => connection.must_reauth )
+		.map( connection => connection.service_name );
+}
